830d07c0f7e4ab1c6a45fb7e88b1baae2b362fbc
Your Name <you@example.com>
2022-09-18 10:34:52 +0500

    basic testing working good but some problem

--- test/BoxTesting.js
+++ test/BoxTesting.js
@@ -1,10 +1,25 @@
 const BoxContract = artifacts.require("Box");
 
 contract("Box Contract" , () => {
-    it(" it should return Box Value ", async () => {
+    it(" it should retuen a store func", async () => {
         const boxContract = await BoxContract.deployed();
-        const result = await boxContract.store(10);
-        console.log(result);
+        const result = await boxContract.store(70); // Working Good
+        // console.log(typeof result.toNumber());
+        // const check = await result.store;
+        // console.log(check)
+
+        // console.log(result);
+        // assert(result === typeof(number));
+
+    });
+
+    it(" it should return retrieve func ", async () => {
+        const boxContract = await BoxContract.deployed();
+        const result = await boxContract.retrieve();
+        
+
+        console.log(result.toNumber());
+        
 
     });
 
